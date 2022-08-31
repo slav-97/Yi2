7849f91bd81f3f210f1ba8effc28cb9106c5dd03
slav-97 <55139626+slav-97@users.noreply.github.com>
2022-08-31 15:12:43 +0300

    Add files via upload

---
+++ s4/models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Post extends ActiveRecord
+{
+
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+}
